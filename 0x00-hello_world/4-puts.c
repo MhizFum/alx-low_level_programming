ddc3bf9df0ed9f80627d773745ff461e62d10c40
mhizfum <mhizfum@gmail.com>
2022-10-13 13:29:32 -0700

    t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 /**
- *main-printing
+ *main - Entry Point
  *
  * Return: Always 0 {Successful)
  */
@@ -9,5 +8,5 @@
 int main (void)
 {
 	puts ("/"Programming is like building a multilingual puzzle");
-	retutn (0);
+	return (0);
 }
