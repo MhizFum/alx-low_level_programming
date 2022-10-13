72e238d56c70ad6122c9dd9983b1db3de71ae4cd
mhizfum <mhizfum@gmail.com>
2022-10-13 15:40:54 -0700

    task *

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
 /**
  * main - prints exactly and that piece of art is useful
  *
- * Return: always 0 (Success)
+ * Return: always 1 (Success)
  */
 
 int main(void)
 {
-	write(2, "and thhat piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
