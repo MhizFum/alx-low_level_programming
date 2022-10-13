f495e0a17c0ef156b1b84174b716e9e59586474b
mhizfum <mhizfum@gmail.com>
2022-10-13 14:48:14 -0700

    wow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#!/bin/bash
-#include <stdio.c>
+#include <stdio.h>
 
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multi
-lingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
