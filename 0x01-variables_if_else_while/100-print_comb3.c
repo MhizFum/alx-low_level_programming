fd71d6cc68c8d3648de581d90d594d0935d4b2f8
mhizfum <mhizfum@gmail.com>
2022-10-16 12:29:07 -0700

    t100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @void: parameter
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i + 48; i < 58; j++)
+	{
+		putchar(i);
+		putchar(j);
+		if (i != 56 && j != 58)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
