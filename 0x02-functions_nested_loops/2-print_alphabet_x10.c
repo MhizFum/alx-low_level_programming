7bd2b7d73ae353dae15b8bfb12ef43a6cea56f13
mhizfum <mhizfum@gmail.com>
2022-10-19 15:18:47 -0700

    tu

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - in lower cases
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+
+}
