5a433609fa0344564a288a0c5f7b039456234fa0
mhizfum <mhizfum@gmail.com>
2022-10-19 15:43:10 -0700

    t4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - check alpha error
+ * @c: character
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'a' && c <= 'z'));
+}
