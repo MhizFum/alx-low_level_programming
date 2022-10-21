90c2b1b23ab3fcfd61d5b41e4b0490bc6458bc81
mhizfum <mhizfum@gmail.com>
2022-10-21 15:38:41 -0700

    mm

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - square
- * @size; size of square
+ * @size: size of square
  */
 
 void print_square(int size)
@@ -14,7 +14,7 @@ void print_square(int size)
 	else
 	{
 		int i, j;
-		
+
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
